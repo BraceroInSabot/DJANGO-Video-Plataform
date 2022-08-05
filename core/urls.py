35757548cbbb36e338977366eebd558f769f8bbe
Guilherme Bracero Gonzales <guibragon@gmail.com>
2022-08-05 18:42:27 -0300

    perf: views routes defined and authentication forms added

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path, include
-from .views import IndexView
+# Authencation libs
+from django.contrib import auth
+from django.contrib.auth import views as auth_views
+
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path("", IndexView.as_view(template_name="index.html"), name="index"),
+    path("", home, name="index"),
+
+    # Authentication
+    path("signup", SignUpView.as_view(), name="signup"),
+    path("login", auth_views.LoginView.as_view(template_name="access/login.html"), name="login"),
+    path("logout", auth_views.LogoutView.as_view(template_name="registration/signup.html"), name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+
+def home(request):
+    return render(request, 'index/index.html')
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
