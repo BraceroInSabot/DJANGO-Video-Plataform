e06f6821a1da85b8c700d9922a645ae766449014
Bracero <guibragon@gmail.com>
2022-10-04 03:52:18 -0300

    fix: secret key and debug missing error solved

--- video/settings.py
+++ video/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", cast=str)
+SECRET_KEY = "django-insecure-$7iba4#ikvg6u#5(029ipi6o8h@4)#&mu_t$yu@m6y=m(s3++c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
